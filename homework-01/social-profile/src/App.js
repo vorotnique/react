f3b72b7c22d71d705f30b3cbca11180212be3d0b
Ruslan Syvokoz <fallenjacket@gmail.com>
2021-06-19 21:29:50 +0300

    hw1 profile react

--- homework-01/social-profile/src/App.js
+++ homework-01/social-profile/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Profile from './components/Profile';
+import user from './user.json';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+		<Profile
+		avatar={user.avatar}
+		name={user.name}
+		tag={user.tag}
+		location={user.location}
+		followers={user.stats.followers}
+		views={user.stats.views}
+		likes={user.stats.likes}
+		/>
+		</div>
+	);
 }
 
 export default App;
